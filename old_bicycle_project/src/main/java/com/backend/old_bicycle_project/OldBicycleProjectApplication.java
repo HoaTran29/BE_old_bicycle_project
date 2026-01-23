6c0949418dfd4f7d17dcf92981c5138c97900f1f
chinhnguyen-eastgate <hoatlkse183433@fpt.edu.vn>
2026-01-24 01:43:40 +0700

    setup BE

---
+++ old_bicycle_project/src/main/java/com/backend/old_bicycle_project/OldBicycleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.backend.old_bicycle_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldBicycleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldBicycleProjectApplication.class, args);
+	}
+
+}
